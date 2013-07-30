d1b3f48ec17e6e7f576f7c8ea9ca5ad375842d96
sam.mazer <sam@samuelmazer.com>
2013-07-29 21:04:38 -0500

    first pass on attribute mod UI
    added metatype lock;
    added attribute value selector and arrow ui;
    
    cleaned up the table drawing and secondary attribute calcuation code;

--- program.js
+++ program.js
@@ -1,7 +1,6 @@
 //JS:TGP testing script
 "use strict";
 
-
 //Hex color randomizer code
 /* 
 var divCode = {
@@ -176,7 +175,63 @@ var charisma = {
 	}
 };
 
+var edge = {
+	"natMax": {
+		"human": 7,
+		"elf": 6,
+		"dwarf": 6,
+		"ork": 6,
+		"troll": 6
+	},
+	"init": {
+		"human": 2,
+		"elf": 1,
+		"dwarf": 1,
+		"ork": 1,
+		"troll": 1
+	}
+};
+
+
 var characterObj = {
+	"metatype": undefined,
+	"points": {
+		"attribute": 0,
+		"skill": 0,
+		"karma": 25,
+		"essence": 6
+	},
+	"condition": {
+		"physical": {
+			"current": 0,
+			"max": 0
+		},
+		"stun": {
+			"current": 0,
+			"max": 0
+		},
+		"initCondition": function () {
+			this.physical.max = Math.ceil((characterObj.attr.body)/2) + 8;
+			this.stun.max = Math.ceil((characterObj.attr.willpower)/2) + 8;
+			this.physical.current = this.physical.max;
+			this.stun.current = this.stun.max;
+		},
+		"getPenalty": function () {	
+			var x, y, result;
+			result = 0;
+			x = this.physical.max - this.physical.current;
+			y = this.stun.max - this.stun.current;
+			while (x >= 3) {
+				x -= 3;
+				result += 1;
+			}
+			while (y >= 3) {
+				y -= 3;
+				result += 1;
+			}
+			return -result;
+		}
+	},
 	"attr": {
 		"body": 0,
 		"agility": 0,
@@ -185,8 +240,23 @@ var characterObj = {
 		"willpower": 0,
 		"logic": 0,
 		"intuition": 0,
-		"charisma": 0
-	},
+		"charisma": 0,
+		"edge": 0,
+		"incAttr": function (attr, x) {
+			if (x > 0) {
+				if (this[attr] < characterObj.maxAttr[attr]) {
+					this[attr] += x;
+				}
+			}
+			if (x < 0) {
+				if (this[attr] > 1) {
+					this[attr] += x;
+				}
+			}
+			refreshCalcs();
+			redrawTables();
+		}
+	},			
 	"maxAttr": {
 		"body": 0,
 		"agility": 0,
@@ -195,26 +265,215 @@ var characterObj = {
 		"willpower": 0,
 		"logic": 0,
 		"intuition": 0,
-		"charisma": 0
+		"charisma": 0,
+	},
+	"secondaryAttr": {
+		"initiative": {
+			"dice": 0,
+			"calcInitiative": function () {
+				this.dice = characterObj.attr.reaction + characterObj.attr.intuition;
+			}
+		},
+		"edge": {
+			"current": 0,
+			"natMax": 0,
+			"initEdge": function(){
+					this.current = edge.init[characterObj.metatype];
+					this.natMax = edge.natMax[characterObj.metatype];
+			}
+		}
+	},
+	"limits": {
+		"mental": 0,
+		"physical": 0,
+		"social": 0,
+		"calcLimits": function () {
+			var attr = characterObj.attr;
+			this.mental = Math.ceil(((attr.logic * 2) + attr.intuition + attr.willpower)/3);
+			this.physical = Math.ceil(((attr.strength * 2) + attr.body + attr.reaction)/3);
+			this.social = Math.ceil(((attr.charisma * 2) + attr.willpower + characterObj.points.essence)/3);
+		}
 	}
 };
 
-var priorities = {
-	"attribute-points": 15
+
+
+//set standard order of attributes
+var attributeArray = ["body", "agility", "reaction", "strength", "willpower", "logic", "intuition", "charisma"];
+var limitArray = ["mental", "physical", "social"];
+
+//jquery lite
+var getTable = function (tableClass) {
+	var array = document.getElementsByClassName(tableClass);
+	return array;
+};
+
+
+
+
+
+
+
+
+//table drawing functions
+var populateAttributeTable = function () {
+	var array;
+	array = getTable("attr");
+	for (var i = 0; i < array.length; i += 1) {
+		array[i].nextSibling.nextSibling.firstChild.nextSibling.innerHTML = characterObj.attr[attributeArray[i]];
+		array[i].nextSibling.nextSibling.nextSibling.nextSibling.innerHTML = characterObj.maxAttr[attributeArray[i]];
+	}
+};
+
+var populateSecondaryAttrTable = function () {
+	var array, i;
+	array = getTable("secondaryAttr");
+	for (i = 0; i < array.length; i += 1) {
+		if (array[i].innerHTML === "Edge") {
+			array[i].nextSibling.nextSibling.firstChild.nextSibling.innerHTML = characterObj.secondaryAttr.edge.current;
+			array[i].nextSibling.nextSibling.nextSibling.nextSibling.innerHTML = characterObj.secondaryAttr.edge.natMax;
+		}
+		else if (array[i].innerHTML === "Initiative") {
+			array[i].nextSibling.nextSibling.innerHTML = characterObj.secondaryAttr.initiative.dice;
+		}
+	}
+};
+
+var populateLimitTable = function () {
+	var array, i;
+	array = getTable("limit");
+	for (i = 0; i < array.length; i +=1) {
+		array[i].nextSibling.nextSibling.innerHTML = characterObj.limits[limitArray[i]];
+	}
+};
+
+var redrawTables = function () {
+	populateAttributeTable();
+	populateSecondaryAttrTable();
+	populateLimitTable();
 }
 
-var rewriteAttrTable = function () {
-	var attrArray = document.getElementsByClassName("attr");
-	var metatype = el.value;
-	for (var i = 0; i < attrArray.length; i += 1) {
-		var currentAttr = eval(attrArray[i].innerHTML.toLowerCase());
-		var currentAttrString = attrArray[i].innerHTML.toLowerCase();
-		characterObj.attr[currentAttrString] = currentAttr.init[metatype];
-		characterObj.maxAttr[currentAttrString] = currentAttr.natMax[metatype];
-		attrArray[i].nextSibling.nextSibling.innerHTML = characterObj.attr[currentAttrString];
-		attrArray[i].nextSibling.nextSibling.nextSibling.nextSibling.innerHTML = characterObj.maxAttr[currentAttrString];
+
+
+
+
+
+//CHARACTEROBJ AND ASSOCIATED CALCUATIONS
+//pull characterObj data from the attribute objects
+//!!!!warning will erase existing stored characterObj data!!!
+var initCharacterObj = function (metatype) {
+	var i;
+	for (i = 0; i < attributeArray.length; i += 1) {
+		characterObj.attr[attributeArray[i]] = window[attributeArray[i]].init[metatype];
+		characterObj.maxAttr[attributeArray[i]] = window[attributeArray[i]].natMax[metatype];
+	}
+};
+
+//all-inclusive attribute-based value refresh
+var refreshCalcs = function () {
+	characterObj.secondaryAttr.initiative.calcInitiative();
+	characterObj.secondaryAttr.edge.initEdge();
+	characterObj.limits.calcLimits();
+	characterObj.condition.initCondition();
+}
+
+//initial population from the metatype selection
+var metatypePopulate = function () {
+	characterObj.metatype = document.getElementById("metatypeSelector").value;
+	initCharacterObj(characterObj.metatype);
+	refreshCalcs();
+	redrawTables();
+	document.getElementById("tablesDiv").className = "";
+
+};
+
+
+
+
+
+
+
+//UI FUNCTIONS
+var lockUnlockMetatype = function () {
+	var x = document.getElementById("metatypeSelector");
+	if (!x.disabled) {
+		x.disabled = true;
+		document.getElementById("metatypeLock").src = "images/locked.png";
+	}
+	else {
+		x.disabled = false;
+		document.getElementById("metatypeLock").src = "images/unlocked.png";
 	}
 }
 
-var el = document.getElementById("metatypeSelector");
-el.addEventListener('change', rewriteAttrTable, false);
+var hideArrows = function (x) {
+	var el = getTable("current");
+	var i;
+	if (x) {
+		x.previousSibling.className = "hidden";
+		x.nextSibling.className = "hidden";
+		return;
+	}
+	for (i = 0; i < el.length; i += 1) {
+		el[i].previousSibling.className = "hidden";
+		el[i].nextSibling.className = "hidden";
+	}
+}
+
+var toggleArrows = function () {
+	var i;
+	var x = document.getElementById("selected");
+	if (this.previousSibling.className === "hidden") {
+		this.id = "selected"
+		this.previousSibling.className = "notHidden";
+		this.nextSibling.className = "notHidden";
+	} 
+	if (x) {
+		x.id = "";
+		hideArrows(x);
+	}
+};
+
+var upAttr = function () {
+	var attr = this.parentNode.previousSibling.previousSibling.innerHTML.toLowerCase();
+	console.log(attr);
+	if (attr === "edge") {
+		if (characterObj.secondaryAttr.edge.current < characterObj.secondaryAttr.edge.natMax) {
+			characterObj.secondaryAttr.edge.current += 1;
+			redrawTables();
+		}
+	}
+	else {
+		characterObj.attr.incAttr(attr, 1);
+	}
+};
+var downAttr = function () {
+	var attr = this.parentNode.previousSibling.previousSibling.innerHTML.toLowerCase();
+	if (attr === "edge") {
+		if (characterObj.secondaryAttr.edge.current > 0) {
+			characterObj.secondaryAttr.edge.current -= 1;
+			redrawTables();
+		}
+	}
+	else {
+		characterObj.attr.incAttr(attr, -1);
+	}
+};
+
+
+
+
+
+//onload event listeners
+var el;
+el = document.getElementById("metatypeSelector");
+el.addEventListener('change', metatypePopulate, false);
+el = document.getElementById("metatypeLock");
+el.addEventListener('click', lockUnlockMetatype, false);
+el = getTable("current");
+for (var i = 0; i < el.length; i += 1) {
+	el[i].addEventListener('click', toggleArrows, false);
+	el[i].previousSibling.addEventListener('click', upAttr, false);
+	el[i].nextSibling.addEventListener('click', downAttr, false);
+}
+hideArrows();
